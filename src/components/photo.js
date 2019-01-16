d6c65052d7aa31fd9c6f595257b120a29cc0bab1
Nathan Toews <nathan.toews@blastradius.com>
2019-01-16 08:23:00 -0500

    single photo component added

---
+++ src/components/photo.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+
+const mapStateToProps = state => {
+  return {};
+};
+
+class SeriesInfo extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+  }
+  componentDidMount() {
+    // this.props.getSeriesFull(this.props.match.params.id);
+  }
+  render() {
+    return (
+      <article>
+        <div className="image" style={{ backgroundImage: `url(${this.props.info.image_url[0]})` }} />
+        <h3 className="title">{this.props.info.name}</h3>
+        <p className="desc" dangerouslySetInnerHTML={{ __html: this.props.info.description }} />
+      </article>
+    );
+  }
+}
+
+const SingleSeries = connect(mapStateToProps)(SeriesInfo);
+
+export default SingleSeries;
